24f5d76c32c823db142308b70bd2f4097098822c
TasukuKuchinomachi <t_kuchinomachi@mozu-inc.com>
2026-04-20 16:28:15 +0900

    feat: resolve @tasukukuchinomachi scope via GitHub Packages
    - default.json: add packageRule that points @tasukukuchinomachi/* to
      https://npm.pkg.github.com so lookups for private GitHub Packages
      succeed.
    - config.js: register hostRules for npm.pkg.github.com using a token
      read from RENOVATE_NPM_REGISTRY_TOKEN so Renovate authenticates
      when querying private packages.
    - workflow: pass the GitHub App installation token to
      RENOVATE_NPM_REGISTRY_TOKEN. The App needs "Packages: Read" added.

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+const npmRegistryToken = process.env.RENOVATE_NPM_REGISTRY_TOKEN;
+
+/** @type {import('renovate/dist/config/types').AllConfig} */
 module.exports = {
   platform: 'github',
   endpoint: 'https://api.github.com/',
@@ -11,4 +14,13 @@ module.exports = {
   gitAuthor: 'nomach-renovate[bot] <nomach-renovate[bot]@users.noreply.github.com>',
   cacheDir: '/tmp/renovate/cache',
   baseDir: '/tmp/renovate',
+  hostRules: npmRegistryToken
+    ? [
+        {
+          hostType: 'npm',
+          matchHost: 'npm.pkg.github.com',
+          token: npmRegistryToken,
+        },
+      ]
+    : [],
 };
